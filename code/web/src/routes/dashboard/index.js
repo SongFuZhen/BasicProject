3c61b6bcb6b655361bb4cf8320eaa21e75cc246c
SongFuZhen <evil_song@126.com>
2017-06-26 18:25:16 +0800

    basic

--- code/web/src/routes/dashboard/index.js
+++ code/web/src/routes/dashboard/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { connect } from 'dva'
 import { Row, Col, Card } from 'antd'
 // import { NumberCard, Quote, Sales, Weather, RecentSales, Comments, Completed, Browser, Cpu, User } from './components'
-// import { User } from './components'
 import styles from './index.less'
 import { color } from '../../utils'
 
--- code/web/src/services/dashboard.js
+++ code/web/src/services/dashboard.js
@@ -9,6 +9,7 @@ export async function myCity (params) {
   })
 }
 
+//http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url=%27http://www.zuimeitianqi.com/zuimei/myCity?flg%3D0%27&format=json&flg=0
 export async function queryWeather (params) {
   return request({
     url: 'http://www.zuimeitianqi.com/zuimei/queryWeather',
